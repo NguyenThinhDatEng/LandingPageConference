e24e6ae1d9b755f8315e8116b2fa6120a762eaf2
NguyenThinhDatEng <thinh.nv194178@sis.hust.edu.vn>
2025-07-10 00:29:05 +0700

    Cập nhật thông tin lưu trú cho phần nội dung có thể tự thay thế

--- src/components/main/AccommodationInfoSection.vue
+++ src/components/main/AccommodationInfoSection.vue
@@ -2,20 +2,26 @@
   <SectionFull :title="$t('accommodation.title')" section-id="accommodation" is-full-width class="accomodation-section">
     <div class="w-full flex flex-col items-center md:flex-row bg-[#fff] overflow-hidden">
       <!-- Left: Text -->
-      <div
-        class="min-h-[300px] sm:min-h-[400px] px-[20px] sm:px-[60px] py-4 md:pl-[160px] md:pr-[40px] sm:py-8 bg-[#232323] md:basis-4/10 md:w-4/10 w-full flex flex-col justify-center text-white md:items-start items-center">
-        <div class="w-full md:w-auto">
-          <div class="font-bold text-2xl mb-8" style="font-family: 'Pacifico', cursive;">
-            {{ contents[currentIndex].title }}
-          </div>
-          <div class="text-base leading-relaxed" v-html="contents[currentIndex].text">
+      <transition name="slide-fade" mode="out-in">
+        <div
+          class="min-h-[300px] sm:min-h-[400px] px-[20px] sm:px-[60px] py-4 md:pl-[160px] md:pr-[40px] sm:py-8 bg-[#232323] md:basis-4/10 md:w-4/10 w-full flex flex-col justify-center text-white md:items-start items-center"
+          :key="currentIndex + '-text'"
+        >
+          <div class="w-full md:w-auto">
+            <div class="font-bold text-2xl mb-8" style="font-family: 'Pacifico', cursive;">
+              {{ contents[currentIndex].title }}
+            </div>
+            <div class="text-base leading-relaxed" v-html="contents[currentIndex].text">
+            </div>
           </div>
         </div>
-      </div>
+      </transition>
       <!-- Right: Image -->
-      <div class="md:basis-6/10 md:w-6/10 w-full h-[350px] md:h-auto">
-        <img :src="contents[currentIndex].img" alt="Accommodation Info" class="w-full h-full object-cover rounded-md" />
-      </div>
+      <transition name="slide-fade" mode="out-in">
+        <div class="md:basis-6/10 md:w-6/10 w-full h-[350px] md:h-auto" :key="currentIndex + '-img'">
+          <img :src="contents[currentIndex].img" alt="Accommodation Info" class="w-full h-full object-cover rounded-md" />
+        </div>
+      </transition>
     </div>
   </SectionFull>
 </template>
@@ -68,5 +74,17 @@ Với kiến trúc tinh tế, không gian linh hoạt, đây là lựa chọn l�
   .container-background {
     padding: unset !important;
   }
+}
+
+.slide-fade-enter-active, .slide-fade-leave-active {
+  transition: all 0.6s cubic-bezier(0.55, 0, 0.1, 1);
+}
+.slide-fade-enter-from {
+  opacity: 0;
+  transform: translateX(100px);
+}
+.slide-fade-leave-to {
+  opacity: 0;
+  transform: translateX(-100px);
 }
 </style>
